f69a61327a5497771676d982a59327aa3818f822
solanalivio <solana.livio1976@gmail.com>
2026-05-26 18:23:43 -0300

    probando

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
     </head>
     <body>
         <div class="container">
-            <h1><span class="dot"></span>Todo está funcionando correctamente</h1>
+            <h1><span class="dot"></span>Todo está OK </h1>
         </div>
     </body>
     </html>
